be783750a1c0c7368970235f13eb4b021eea809c
jandev <jandev66@gmail.com>
2025-04-19 21:18:40 +0530

    changes in user and admin controller

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 
 import express from "express"
 import { adminLogin, checkadmin } from "../controllers/adminController.js"
@@ -48,55 +48,5 @@ router.get('/checkadmin',authAdmin,checkadmin)
 router.put('/deactivateUser/:userId',authAdmin)
 // address update
 
-=======
 
-import express from "express"
-import { adminLogin, checkadmin } from "../controllers/adminController.js"
-import { authAdmin } from "../middlewares/authAdmin.js"
-import { adminProfile } from "../controllers/adminController.js"
-import { adminProfileUpdate } from "../controllers/adminController.js"
-import { adminLogout } from "../controllers/adminController.js"
-import { signup } from "../controllers/adminController.js"
-import { getAllAdmins,updateAdminById } from "../controllers/adminController.js"
-import Admin from "../models/adminModel.js"
-
-
-const router = express.Router()
-
-//sign up
-router.post("/signup",authAdmin,signup)
-// login 
-router.post("/login",adminLogin)
-// profile
-router.get("/profile",authAdmin,adminProfile)
-// profile edit
-router.put("/update",authAdmin,adminProfileUpdate)
-
-router.get('/all',authAdmin, getAllAdmins)
-
-
-router.delete("/:id", authAdmin, async (req, res) => {
-    const admin = await Admin.findByIdAndDelete(req.params.id);
-    if (!admin) return res.status(404).json({ message: "Admin not found" });
-    res.json({ message: "Admin deleted successfully" });
-  });
-
-  router.put('/:id', authAdmin, updateAdminById);
-
-
-// profile deactivate
-router.put("/deactivate",authAdmin)
-
-//logout
-router.get("/logout",authAdmin,adminLogout)
-
-router.delete('/delete-account')
-//password-forgot
-router.get('/checkadmin',authAdmin,checkadmin)
-
-// password change
-router.put('/deactivateUser/:userId',authAdmin)
-// address update
-
->>>>>>> 00d43a0 (archived file push)
 export default router
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 
 import express from "express"
 import { checkUser, signup, userLogin, userLogout, userProfile, userProfileUpdate } from "../controllers/userController.js"
@@ -54,61 +54,5 @@ router.get("/",authAdmin, async (req, res) => {
   });
 
 
-=======
 
-import express from "express"
-import { checkUser, signup, userLogin, userLogout, userProfile, userProfileUpdate } from "../controllers/userController.js"
-import { authUser } from "../middlewares/authUser.js"
-import { authAdmin } from "../middlewares/authAdmin.js"
-import User from "../models/userModel.js"
-import {upload} from "../middlewares/multer.js"
-
-const router = express.Router()
-
-//sign up
-router.post("/signup",signup)
-// login 
-router.post("/login",userLogin)
-// profile
-router.get("/profile",authUser,userProfile)
-// profile edit
-router.put("/update",authUser,upload.single("profilePic"), userProfileUpdate)
-
-// profile deactivate
-router.put("/deactivate",authAdmin)
-
-//logout
-router.get("/logout",authUser,userLogout)
-
-router.delete('/delete-account')
-//password-forgot
-router.get('/checkuser',authUser,checkUser)
-
-// password change
-router.put('/deactivateUser/:userId',authAdmin)
-
-router.get("/recent",authAdmin, async (req, res) => {
-    const users = await User.find().sort({ createdAt: -1 }).limit(5);
-    res.json(users);
-  });
-// address update
-router.get("/",authAdmin, async (req, res) => {
-    const users = await User.find().sort({ createdAt: -1 });
-    res.json(users);
-  });
-
-  router.get('/me', (req, res) => {
-    const token = req.cookies?.token;
-    if (!token) return res.status(401).json({ message: 'Not authenticated' });
-  
-    try {
-      const user = jwt.verify(token, process.env.JWT_SECRET); // or however you're verifying
-      res.json({ data: user }); // or return user data from DB
-    } catch (err) {
-      return res.status(401).json({ message: 'Invalid or expired token' });
-    }
-  });
-
-
->>>>>>> 00d43a0 (archived file push)
 export default router
